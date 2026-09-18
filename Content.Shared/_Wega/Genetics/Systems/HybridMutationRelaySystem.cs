d0f3a81140c4a9f109e4aa4d63a67c09a2b3f5d6
guilhermegalinhaazul-droid <guilhermegalinhaazul@gmail.com>
2026-09-17 23:56:25 -0300

    Fix hybrid mutation relay damage namespace

--- Content.Shared/_Wega/Genetics/Systems/HybridMutationRelaySystem.cs
+++ Content.Shared/_Wega/Genetics/Systems/HybridMutationRelaySystem.cs
@@ -3,7 +3,7 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 using Content.Shared.Body.Events;
-using Content.Shared.Damage.Systems;
+using Content.Shared.Damage;
 using Content.Shared.Flash;
 using Content.Shared.Mobs;
 using Content.Shared.Speech;
