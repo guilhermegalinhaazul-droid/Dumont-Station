d1e404844a804698ba0a396e289d9b4f0c20d016
guilhermegalinhaazul-droid <guilhermegalinhaazul@gmail.com>
2026-09-17 23:56:07 -0300

    Fix Trauma injection FixedPoint namespace

--- Content.Trauma.Shared/Genetics/Abilities/InjectChemicalsActionComponent.cs
+++ Content.Trauma.Shared/Genetics/Abilities/InjectChemicalsActionComponent.cs
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-using Content.Shared.FixedPoint;
+using Content.Goobstation.Maths.FixedPoint;
 using Content.Shared.Actions;
 using Content.Shared.Chemistry.Reagent;
 using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
