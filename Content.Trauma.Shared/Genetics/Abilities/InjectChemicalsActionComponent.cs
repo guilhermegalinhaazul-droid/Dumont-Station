030038acdc340d8854ae2c1a3ada626f2b7cf1fb
guilhermegalinhaazul-droid <guilhermegalinhaazul@gmail.com>
2026-09-18 00:01:53 -0300

    Allow server-side hybrid mutation system adapter

--- Content.Trauma.Shared/Genetics/Abilities/InjectChemicalsActionComponent.cs
+++ Content.Trauma.Shared/Genetics/Abilities/InjectChemicalsActionComponent.cs
@@ -11,7 +11,7 @@ namespace Content.Trauma.Shared.Genetics.Abilities;
 /// Adds reagents to the user's bloodstream, then after a comedown period adds different reagents.
 /// This must be added to an action entity, with <c>raiseOnAction: true</c>
 /// </summary>
-[RegisterComponent, NetworkedComponent, Access(typeof(InjectChemicalsActionSystem))]
+[RegisterComponent, NetworkedComponent]
 [AutoGenerateComponentPause]
 public sealed partial class InjectChemicalsActionComponent : Component
 {
