58177847a7ebce9973e0578ed9a4bea16002139d
guilhermegalinhaazul-droid <guilhermegalinhaazul@gmail.com>
2026-09-18 18:36:33 -0300

    Fix hybrid genetics client Control imports

--- Content.Client/_Wega/Genetics/Ui/DnaModifierWindow.Ei.cs
+++ Content.Client/_Wega/Genetics/Ui/DnaModifierWindow.Ei.cs
@@ -5,6 +5,7 @@
 using Content.Client.Stylesheets;
 using Content.Shared.Genetics;
 using Content.Shared.Genetics.UI;
+using Robust.Client.UserInterface;
 using Robust.Client.UserInterface.Controls;
 
 namespace Content.Client._Wega.Genetics.Ui;
