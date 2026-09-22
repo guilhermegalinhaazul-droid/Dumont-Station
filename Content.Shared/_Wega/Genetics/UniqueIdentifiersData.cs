038e7592060708bc02ee8f9c95f8c12ccaaf8425
guilhermegalinhaazul-droid <guilhermegalinhaazul@gmail.com>
2026-09-22 20:52:56 +0000

    Fix genetics appearance access permissions

--- Content.Shared/_Wega/Genetics/UniqueIdentifiersData.cs
+++ Content.Shared/_Wega/Genetics/UniqueIdentifiersData.cs
@@ -4,7 +4,7 @@ using Robust.Shared.Serialization;
 namespace Content.Shared.Genetics;
 
 [Serializable, NetSerializable]
-[Access(typeof(SharedDnaModifierSystem), typeof(EnzymeInfo))]
+[Access(typeof(SharedDnaModifierSystem), typeof(EnzymeInfo), typeof(AppearanceGene))]
 public sealed class UniqueIdentifiersData
 {
     public string? EntityName { get; set; }
